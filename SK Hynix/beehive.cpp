f42303c49a7b46278e6d33802c45ce83d2389b05
jkjan <ytom2607@gmail.com>
2022-03-31 00:00:07 +0900

    SK

---
+++ SK Hynix/beehive.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#define MAX_N 100
+
+using namespace std;
+
+int main() {
+    int n;
+    int arr[MAX_N][MAX_N] = {0, };
+    scanf(" %d", &n);
+
+    int k = n;
+    int y = 0;
+    int x = k / 2;
+    int num = 1;
+
+    while (k > 1) {
+        for (int i = 0; i < (k - 1) / 2; ++i) {
+            arr[y][x] = num++;
+            y += 1; x += 1;
+        }
+        for (int i = 0; i < (k - 1) / 2; ++i) {
+            arr[y][x] = num++;
+            y += 1; x -= 1;
+        }
+        for (int i = 0; i < (k - 1) / 2; ++i) {
+            arr[y][x] = num++;
+            y += 0; x -= 1;
+        }
+        for (int i = 0; i < k - 1; ++i) {
+            arr[y][x] = num++;
+            y -= 1; x += 0;
+        }
+        for (int i = 0; i < (k - 1) / 2 - 1; ++i) {
+            arr[y][x] = num++;
+            y += 0; x += 1;
+        }
+        arr[y][x] = num++;
+        k -= 2;
+        y += 1; x += 1;
+    }
+    arr[y][x] = num;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            printf("%2d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    y = 0;
+    x = n / 2;
+    int half = (n + 1) / 2;
+    for (; y < half; y++) {
+        for (int j = x; j >= 0; j--) {
+            printf("%d %d\n", y, x);
+//            printf("%d ", arr[y][x]);
+        }
+        x += 1;
+    }
+    for (; y < n; y++) {
+        for (int j = x; j >= 0; j--) {
+            printf("%d %d\n", y, x);
+//            printf("%d ", arr[y][x]);
+        }
+        x -= 1;
+    }
+
+    return 0;
+}
